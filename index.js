153d250bf6226146e479301901c5fe3e5d73021a
Meghana Velagala <meghanavelagala999@gmail.com>
2023-08-18 21:45:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,57 @@
+var i=0;
+while(i<=6){
+document.querySelectorAll("button")[i].addEventListener("click", function () {
+    var buttonLetter = this.innerHTML;
+    action(buttonLetter);
+    respond(buttonLetter)
+})
+i=i+1;
+}
+
+document.addEventListener("keydown",function(event){
+    action(event.key)
+    respond(event.btpress)
+})
+
+function action(key){
+    switch (key) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            break;
+        case "a":
+               var tom2 = new Audio("sounds/tom-2.mp3");
+               tom2.play();
+                break;
+                case "s":
+                    var tom3 = new Audio("sounds/tom-3.mp3");
+                    tom3.play();
+                     break;
+                     case "d":
+                        var tom4 = new Audio("sounds/tom-4.mp3");
+                        tom4.play();
+                         break;  
+                         case "j":
+                    var snare = new Audio("sounds/snare.mp3");
+                    snare.play();
+                     break; 
+                     case "k":
+                        var bass = new Audio("sounds/kick-bass.mp3");
+                        bass.play();
+                         break;
+                         case "l":
+                            var crash = new Audio("sounds/crash.mp3");
+                            crash.play();
+                             break; 
+        default:
+        console.log(this.innerHTML)
+    }
+}
+
+function respond(btpress){
+    var mode=document.querySelector("."+btpress)
+mode.classList.add("pressed");
+setTimeout(function(){
+    mode.classList.remove("pressed");
+},200)
+}
